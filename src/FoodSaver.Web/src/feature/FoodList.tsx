ecc6a9c0652d72eff93470652e74eb09c5bd6089
Eric Faraut <eric.faraut@hotmail.fr>
2026-05-16 13:31:30 +0200

    feat(food-list): show consumed state

--- src/FoodSaver.Web/src/feature/FoodList.tsx
+++ src/FoodSaver.Web/src/feature/FoodList.tsx
@@ -9,15 +9,31 @@ export function FoodList({ foods, onConsume }: Props) {
   return (
     <ul>
       {foods.map((food) => (
-        <li key={food.id}>
+        <li 
+          key={food.id} 
+          className={food.isConsumed ? 'consumed' : ''}
+          aria-label={`
+            ${food.name},
+            ${food.isConsumed 
+            ? 'consumed'
+            : 'active' }
+          `}
+        >
           <span>
             {food.name} - {food.expiryDate}
           </span>
 
           {!food.isConsumed && (
-            <button onClick={() => onConsume(food.id)}>
+            <button 
+              onClick={() => onConsume(food.id)}
+              aria-label='Mark food as consumed'
+            >
               Consume
             </button>
+          )}
+
+          {food.isConsumed && (
+            <small>Consumed</small>
           )}
         </li>
       ))}
