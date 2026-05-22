db501bba68a94d6724933710c6660df026aaa721
Eric Faraut <eric.faraut@hotmail.fr>
2026-05-22 12:43:01 +0200

    test(e2e): initalize core food workflow scenario

---
+++ src/FoodSaver.Web/cypress/e2e/food-workflow.cy.ts
@@ -0,0 +1,42 @@
+describe('food workflow', () => {
+  it('must create and consume a food item', () => {
+    // Arrange
+    const foodName = `Milk (test e2e ${new Date().toLocaleString('fr-FR').replace(',', '')}`;
+
+    cy.request({
+      url: 'https://foodsaver-api-00tb.onrender.com/foods',
+      retryOnStatusCodeFailure: true,
+      timeout: 30000,
+    });
+    cy.visit('/');
+
+    // Act
+    cy.findByPlaceholderText(/food name/i)
+      .type(foodName);
+
+    cy.get('input[type="date"]')
+      .type('2026-12-31');
+
+    cy.get('input[type="number"]')
+      .clear()
+      .type('2');
+
+    cy.findByRole('button', {
+      name: /add/i,
+    }).click();
+
+    // Assert
+    cy.contains('li', foodName)
+      .should('exist')
+      .first()
+      .as('foodItem');
+
+    cy.get('@foodItem')
+      .find('button', { timeout: 10000 })
+      .contains(/consume/i)
+      .click();
+
+    cy.get('@foodItem')
+      .should('contain.text', /consumed/i);
+  });
+});
